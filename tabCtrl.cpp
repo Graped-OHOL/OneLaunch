de51761f334405fc6111dd12389b4a190d6cd3de
Graped - OHOL <maineiac0001@gmail.com>
2024-09-22 10:16:39 -0400

    getTabButtonFrame(index) used to recolor the frame of a tab button when its the active tab.

--- tabCtrl.cpp
+++ tabCtrl.cpp
@@ -18,6 +18,7 @@ void Tabs::init() {
     mainWindow->tabs.Set(0);
 
     mainWindow->LauncherTabBtn.SetStyle(Button::StyleLeftEdge());
+    mainWindow->LauncherTabBtnFrame.Background(Color(0, 160, 225));
 
     mainWindow->LauncherTabBtn.WhenPush = [=] { switchTab(0); };
     mainWindow->AccountManagerTabBtn.WhenPush = [=] { switchTab(1); };
@@ -30,12 +31,14 @@ void Tabs::switchTab(int tabIndex) {
     for (int i = 0; i < 3; ++i) {
 
         getTabButton(i)->SetStyle(Button::StyleNormal());
+        getTabButtonFrame(i)->Background(Color(114, 114, 114));
     }
 
     if (tabIndex >= 0 && tabIndex < 3) {
 
         mainWindow->tabs.Set(tabIndex);
         getTabButton(tabIndex)->SetStyle(Button::StyleLeftEdge());
+        getTabButtonFrame(tabIndex)->Background(Color(0, 160, 225));
     }
 }
 
@@ -47,6 +50,16 @@ Button* Tabs::getTabButton(int index) {
         case 0: return &mainWindow->LauncherTabBtn;
         case 1: return &mainWindow->AccountManagerTabBtn;
         case 2: return &mainWindow->SettingsTabBtn;
+        default: return nullptr;
+    }
+}
+RichTextCtrl* Tabs::getTabButtonFrame(int index) {
+
+    switch (index) {
+        
+        case 0: return &mainWindow->LauncherTabBtnFrame;
+        case 1: return &mainWindow->AccountManagerTabBtnFrame;
+        case 2: return &mainWindow->SettingsTabBtnFrame;
         default: return nullptr;
     }
 }
